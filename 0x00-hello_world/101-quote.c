2eb14d2f2bd8c32edc05b9f2fdf12a40519256bc
ranahisham995 <ranahisham598@gmail.com>
2023-04-20 17:56:11 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print code without using printf or puts
+ *
+ * return: 1 (fail)
+*/
+
+int main(void)
+{
+	char sen[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(1.sen.59);
+	return (1);
+}
