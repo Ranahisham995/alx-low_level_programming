d5bd7a5d7450702a14c2cc29580cb80a9910d499
ranahisham995 <ranahisham598@gmail.com>
2023-04-21 13:29:07 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print single numbers using putchar
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		/*convert digit to ASCII representation*/
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+
+	return(0);
+}
