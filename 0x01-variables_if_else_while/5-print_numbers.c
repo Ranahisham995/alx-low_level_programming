8a4f931feb58aa7f00ed621552fbe26cb749f701
ranahisham995 <ranahisham598@gmail.com>
2023-04-21 13:20:38 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all the alphabet letters
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+}
